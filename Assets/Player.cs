3d28c54538a4e772ee0fb0825ae8052079781300
ElonKuff <49843275+ElonKuff@users.noreply.github.com>
2019-04-21 21:53:56 +0200

    d

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
- CharacterController characterController;
 
+    CharacterController characterController;
+    Animator animator;
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    public float deadzone = 0.3f;
 
     private Vector3 moveDirection = Vector3.zero;
 
     void Start(){
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     void Update(){
@@ -21,19 +24,29 @@ public class Player : MonoBehaviour
             // We are grounded, so recalculate
             // move direction directly from axes
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            moveDirection *= speed;
+            if(Mathf.Abs(Input.GetAxis("Horizontal"))>deadzone||Mathf.Abs(Input.GetAxis("Vertical"))>deadzone){
+                animator.SetBool("isWalking", true);
+                moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+                moveDirection *= speed;
+            } else {
+                animator.SetBool("isWalking", false);
+            }
+
 
             //if (Input.GetButton("Jump"))
             //{
-             //   moveDirection.y = jumpSpeed;
+            //   moveDirection.y = jumpSpeed;
             //}
+
+            if(Input.GetButton("Fire1")){
+                Debug.Log("Fire");
+            }
         
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
         // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
         // as an acceleration (ms^-2)
-        //moveDirection.y -= gravity * Time.deltaTime;
+        moveDirection.y -= gravity * Time.deltaTime;
 
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
